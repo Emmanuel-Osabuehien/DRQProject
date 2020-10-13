0ffbab6bca1564e600063a64828a981a1b69d37d
EMMANUEL OSABUEHIEN <G00373559@gmit.ie>
2020-10-13 17:18:01 +0100

    Lab Upload

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>This is a footer component.</h1>
+            </div>
+        );
+    }
+}
