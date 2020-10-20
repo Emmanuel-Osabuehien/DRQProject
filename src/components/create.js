23a89c68a1037a47e7a3466b65f2cffbf589214d
EMMANUEL OSABUEHIEN <G00373559@gmit.ie>
2020-10-20 10:24:34 +0100

    Lab 3 Complete

---
+++ src/components/create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Create extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>This is a create component.</h1>
+            </div>
+        );
+    }
+}
