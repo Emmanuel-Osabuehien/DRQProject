05b79f9b7cab7c16806cf37f83e904b802053c7d
EMMANUEL OSABUEHIEN <G00373559@gmit.ie>
2020-11-30 11:52:10 +0000

    Final Upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,7 @@ const port = 4000
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(cors());
 
@@ -14,6 +15,9 @@ app.use(function (req, res, next) {
         "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -100,6 +104,10 @@ app.post('/api/movies', (req, res) => {
     })
 
     res.send('Item Added');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
 })
 
 app.listen(port, () => {
